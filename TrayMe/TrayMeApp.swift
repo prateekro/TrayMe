2f86111b28119706f7c95e760bf66c5c0933d113
Prateek Rokadiya <prateekro@gmail.com>
2025-11-17 23:24:44 -0800

    Initial Commit

---
+++ TrayMe/TrayMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrayMeApp.swift
+//  TrayMe
+//
+//  Created by Prateek Rokadiya on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TrayMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
